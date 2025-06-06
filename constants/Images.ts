42a2b99f63cc095cb1904c9006a7ba5bc3fd54bb
Hephzibah <hephziboy007@gmail.com>
2025-06-07 02:09:44 +0400

    Updated splash icon and fixed Expo config

--- constants/Images.ts
+++ constants/Images.ts
@@ -1,3 +1,4 @@
+import AddMenu from "@/assets/images/AddMenu.png";
 import afangSoup from "@/assets/images/afangSoup.jpg";
 import bed from "@/assets/images/bed.png";
 import Chefs from "@/assets/images/Chefs.png";
@@ -5,12 +6,12 @@ import Chicken from "@/assets/images/Chicken.png";
 import egusiSoup from "@/assets/images/egusiSoup.jpg";
 import female from "@/assets/images/female.png";
 import Flow2 from "@/assets/images/Flow2.png";
+import gptOrdaLogo from "@/assets/images/gptOrdaLogo.png";
 import JollofChicken from "@/assets/images/JollofChicken.jpg";
 import OrdaLogo from "@/assets/images/OrdaLogo.png";
 import pexelsFood from "@/assets/images/pexelsFood.jpg";
 import room1 from "@/assets/images/room1.jpg";
 import room2 from "@/assets/images/room2.jpg";
-import AddMenu from "@/assets/images/AddMenu.png";
 
 
 
@@ -28,5 +29,7 @@ export default {
     room1,
     room2,
     AddMenu,
+    gptOrdaLogo,
+
 
 };
